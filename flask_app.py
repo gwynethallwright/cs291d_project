075d03a0542378d0af6267d82735ab3177089f1b
gwynethallwright <gwyneth@ucsb.edu>
2020-11-10 19:00:17 -0800

    Add /blockchain route.

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,15 @@
 from flask import Flask, request
 import requests
+import simplejson as json
 from main import BlockChain
 
 flask_app =  Flask(__name__)
 blockchain = BlockChain()
+
+
+@flask_app.route('/blockchain', methods=['GET'])
+def get_blockchain():
+    blockchain_data = []
+    for block in blockchain.blocks:
+        blockchain_data.append(block.__dict__)
+    return json.dumps({'length': len(blockchain_data), 'blocks': blockchain_data})
