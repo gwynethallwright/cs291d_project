0d270b001eec6956d8b6dfafcf8814636cfb4da9
gwynethallwright <gwyneth@ucsb.edu>
2020-11-10 19:33:29 -0800

    Add /submit_transaction route.

--- flask_app.py
+++ flask_app.py
@@ -13,3 +13,29 @@ def get_blockchain():
     for block in blockchain.blocks:
         blockchain_data.append(block.__dict__)
     return json.dumps({'length': len(blockchain_data), 'blocks': blockchain_data})
+
+
+@flask_app.route('/new_transaction', methods=['POST'])
+def new_transaction():
+    transaction_data = request.get_json()
+    required_fields = ['sender', 'receiver', 'amount']
+
+    for field in required_fields:
+        if not transaction_data.get(field):
+            return 'Invalid transaction data.', 404
+
+    blockchain.add_new_transaction(transaction_data)
+    return "Success.", 201
+
+
+@flask_app.route('/submit_transaction', methods=['POST'])
+def submit_textarea():
+    transaction_sender = request.form['sender']
+    transaction_receiver = request.form['receiver']
+    transaction_amount = request.form['amount']
+
+    transaction_to_post = {'sender': transaction_sender, 'receiver': transaction_receiver, 'amount': transaction_amount}
+    new_transaction_address = "{}/new_transaction".format(NODE_ADDRESS)
+    requests.post(new_transaction_address, json=transaction_to_post, headers={'Content-type': 'application/json'})
+
+    return redirect('/')
