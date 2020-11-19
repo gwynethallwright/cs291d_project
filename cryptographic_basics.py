845de6306ab24cdba984794622ac21b3200c98bc
gwynethallwright <gallwright@invoca.com>
2020-11-18 19:13:41 -0800

    Add stubs for cryptographic basics.

---
+++ cryptographic_basics.py
@@ -0,0 +1,26 @@
+"""
+def G_sig(security_parameter):
+
+
+def K_sig(pp_sig):
+
+
+def S_sig(sk_sig, m):
+
+
+def V_sig(pk_sig, m, sigma):
+
+
+def G_enc(security_parameter):
+
+
+def K_enc(pp_enc):
+
+
+def E_enc(pk_enc, m):
+
+
+def D_enc(sk_enc, c):
+"""
+
+
